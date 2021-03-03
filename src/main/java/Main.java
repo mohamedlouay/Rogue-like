2c360084ec5a260082fb652f2bf8c82624196981
louay belhassen <belhassenloulou@yahoo.fr>
2021-03-03 23:51:01 +0100

    creer la classe

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,24 @@
+
+import java.io.IOException;
+        import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello word");
+
+
+
+
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner scanner = new Scanner(System.in);
+        String fileName ;
+        System.out.println("choisissez une map parmi : map1.txt map2.txt map3.txt");
+        fileName = scanner.next();
+        GameSystem gameSystem = new GameSystem(fileName) ;
+        gameSystem.playGame();
+
+
     }
+
+
 }
