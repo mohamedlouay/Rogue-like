cb30c7667410bcc28298e0ba3136fcff8f7ed4a1
kharroubi abdelkader <k.abdelkader1996@gmail.com>
2021-03-07 11:27:05 +0100

    add player

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,24 +1,5 @@
-
-import java.io.IOException;
-        import java.util.Scanner;
-
 public class Main {
-
-
-
-
-
-    public static void main(String[] args) throws IOException {
-
-        Scanner scanner = new Scanner(System.in);
-        String fileName ;
-        System.out.println("choisissez une map parmi : map1.txt map2.txt map3.txt");
-        fileName = scanner.next();
-        GameSystem gameSystem = new GameSystem(fileName) ;
-        gameSystem.playGame();
-
-
+    public static void main(String[] args) {
+        System.out.println("Hello word");
     }
-
-
 }
---
+++ src/main/java/player.java
@@ -0,0 +1,27 @@
+class Player {
+
+    private int x;
+    private int y;
+
+
+    //initialiser le joueur au position zero
+    public Player() {
+        this.x =0 ;
+        this.y = 0 ;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    //changer la position du joueur
+    void setPosition(int x ,int y)
+    {
+        this.x=x;
+        this.y=y;
+    }
+}
