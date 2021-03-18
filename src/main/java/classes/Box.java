091c2d013fa82e2105eb882f67e1e84fd174423c
kharroubi abdelkader <k.abdelkader1996@gmail.com>
2021-03-18 19:48:36 +0100

    modifier la classe Box

--- src/main/java/classes/Box.java
+++ src/main/java/classes/Box.java
@@ -1,4 +1,27 @@
 package classes;
 
+import java.util.Random;
+
 public class Box {
+    int x;
+    int y;
+    int valeur;
+    Box(int x,int y){
+        this.x=x;
+        this.y=y;
+        Random r = new Random();
+        this.valeur= r.nextInt((100 - 10) + 1) + 10;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getValeur() {
+        return valeur;
+    }
 }
