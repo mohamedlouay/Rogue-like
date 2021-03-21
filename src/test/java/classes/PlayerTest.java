340982f62745869d2d3d0cbca3330d8f19554c59
louay belhassen <belhassenloulou@yahoo.fr>
2021-03-21 23:16:26 +0100

    tests

---
+++ src/test/java/classes/PlayerTest.java
@@ -0,0 +1,28 @@
+package classes;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerTest {
+
+    @Test
+    void deplacerADroite() {
+    }
+
+    @Test
+    void deplacerAGauche() {
+    }
+
+    @Test
+    void deplacerEnHaut() {
+    }
+
+    @Test
+    void deplacerEnBas() {
+    }
+
+    @Test
+    void actualiser() {
+    }
+}
