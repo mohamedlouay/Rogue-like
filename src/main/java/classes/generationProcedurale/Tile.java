bbc6f20fc6f2c2b0927c3cb18eef4c850a6a4df3
louay belhassen <belhassenloulou@yahoo.fr>
2021-03-21 22:59:20 +0100

    creation de l'enumartion Tile

---
+++ src/main/java/classes/generationProcedurale/Tile.java
@@ -0,0 +1,14 @@
+package classes.generationProcedurale;
+/*
+public enum Tile {
+    FLOOR('.') ,WALL(' ');
+
+    private char caractere ;
+    public char getCaractere() { return caractere; }
+
+    Tile(char caractere)
+            {
+                this.caractere=caractere;
+            }
+}
+*/
