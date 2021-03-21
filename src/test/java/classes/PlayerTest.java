535f5f9d671bca076cb57e33150a34cae2600e6b
louay belhassen <belhassenloulou@yahoo.fr>
2021-03-21 23:25:38 +0100

    tests

--- src/test/java/classes/PlayerTest.java
+++ src/test/java/classes/PlayerTest.java
@@ -6,6 +6,11 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class PlayerTest {
 
+
+    Camera camera= new Camera(0,0,200,40);
+    Monde monde=new Monde(500,500);
+    Player player = new Player(11,6,monde);
+
     @Test
     void deplacerADroite() {
     }
