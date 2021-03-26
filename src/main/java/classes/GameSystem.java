abf1c9d929e27c8ac1146bd188a54b779ee9aa73
kharroubi abdelkader <k.abdelkader1996@gmail.com>
2021-03-26 10:12:24 +0100

    update de GameSystem

--- src/main/java/classes/GameSystem.java
+++ src/main/java/classes/GameSystem.java
@@ -1,4 +1,20 @@
 package classes;
 
+import classes.scenes.PlayScene;
+import classes.scenes.Scene;
+
 public class GameSystem {
+
+    private Scene scene ;
+    GameSystem(){
+        scene= new PlayScene();
+    }
+
+    public void setScene(Scene scene) {
+        this.scene = scene;
+    }
+    public Scene getScene() {
+        return scene;
+    }
+
 }
