65200463f12f5240e514829ae2f944bd82920909
louay belhassen <belhassenloulou@yahoo.fr>
2021-04-18 12:27:53 +0200

    tester la classe ConstructeurDuMonde

---
+++ src/test/java/generationProcedurale/ConstructeurDuMondeTest.java
@@ -0,0 +1,73 @@
+package generationProcedurale;
+
+import creatures.Player;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConstructeurDuMondeTest {
+
+    ArrayList<Room> rooms = new ArrayList<Room>();
+    Player player = new Player();
+    ConstructeurDuMonde world = new ConstructeurDuMonde(player,100,300,4,10,15);
+
+
+    @Test
+    void placeRooms() {
+
+        for (int i = 0 ; i< world.rooms.size();i++)
+        {
+            for (int j = i+1  ;j < world.rooms.size();j++)
+            {
+                // assert that all the rooms are disjointed
+                assertFalse(world.rooms.get(j).intersects(world.rooms.get(i)));
+            }
+        }
+
+
+
+
+    }
+
+
+    @Test
+    void initPlayerPosition() {
+
+        //assert that the player is the midlle of room 1
+        assertTrue(player.getPositionX() == world.rooms.get(0).getCenter().getCenterX());
+        assertTrue(player.getPositionY() == world.rooms.get(0).getCenter().getCenterY());
+
+    }
+
+    @Test
+    void initEnemiesPosition() {
+        Boolean found ;
+
+        for (Room r : world.rooms) {
+            found =false ;
+            outerloop:
+            for (int i = r.getY1(); i < r.getY2(); i++) {
+                for (int j = r.getX1() ; j< r.getX2(); j++) {
+                    if ("ZDL".indexOf(world.tiles[i][j].getSymbole() ) != -1){
+
+                        found = true ;
+                        System.out.println("i "  + i + " j  "+ j);
+                        System.out.println("------");
+                        break outerloop ;
+
+                    }
+
+                }
+            }
+
+            assertTrue(found);//assert than every room cantain a enemy
+
+        }
+
+
+    }
+
+
+}
